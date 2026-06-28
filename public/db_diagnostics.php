dde4fff81cf07c91fc7080200d3f7e2b18a6a036
Simplix Deployer <deploy@repuestoscajica.com>
2026-06-28 11:31:41 -0500

    Add database diagnostics script

--- public/db_diagnostics.php
+++ public/db_diagnostics.php
@@ -5,7 +5,18 @@ if (is_dir($dir)) {
     $files = scandir($dir);
     foreach ($files as $f) {
         if (strpos($f, 'tenant_') === 0) {
-            echo "$f: " . filesize($dir . '/' . $f) . " bytes\n";
+            $path = $dir . '/' . $f;
+            $size = 'unknown';
+            try {
+                $size = @filesize($path);
+                if ($size === false) {
+                    $err = error_get_last();
+                    $size = 'error (' . ($err ? $err['message'] : 'unknown') . ')';
+                }
+            } catch (Exception $e) {
+                $size = 'exception: ' . $e->getMessage();
+            }
+            echo "$f: $size bytes\n";
         }
     }
 } else {
